756ee8d0382a5bc50378c9fcc8da9c1c9fdb1b47
Andrey <aandreevh@gmail.com>
2021-01-16 12:50:32 +0200

    Fixed mysql comp

--- server/migrations/20210115221536_add-messages.ts
+++ server/migrations/20210115221536_add-messages.ts
@@ -2,11 +2,12 @@ import * as Knex from "knex";
 
 
 export async function up(knex: Knex): Promise<void> {
+  await knex.schema.dropTableIfExists('messages');
   await knex.schema.createTable('messages', table => {
     table.increments('id').primary();
-    table.integer('user_id').notNullable();
+    table.integer('user_id').unsigned().notNullable();
     table.string('message').notNullable();
-    table.foreign('user_id', 'message_user').references('id').inTable('users');
+    table.foreign('user_id').references('id').inTable('users');
     table.timestamp("created_at").notNullable();
     table.timestamp("updated_at").notNullable();
   });
